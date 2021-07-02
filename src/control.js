2e7dfddee3c1a4e9ee19c26e2adde65f3119bd54
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-07-02 19:49:20 -0300

    Refactor control panel

--- src/control.js
+++ src/control.js
@@ -2,24 +2,50 @@ import save from './save';
 
 
 export default function (filename, settings, vertices, areas, warn) {
-    function createButton(text, click) {
+    function createButton(text) {
         const button = document.createElement('button');
         button.style.width = 'min-content';
         button.style.margin = '.5rem';
+        button.style.fontSize = '11px';
+        button.style.fontFamily = 'Helvetica Neue, Helvetica, Arial, sans-serif';
+        button.style.lineHeight = 1;
         button.innerHTML = text;
-        button.addEventListener('click', click);
         return button;
     }
 
-    const downloadButton = createButton('Save Network', () => {
-        try {
-            save(filename, settings, vertices, areas);
-        } catch (error) {
-            warn(error);
-        }
+    const animationButton = createButton('Load Animation');
+    animationButton.addEventListener('click', () => {
     });
 
+    const settingsButton = createButton('Load Settings');
+    settingsButton.addEventListener('click', () => {
+    });
+
+    const networkButton = createButton('Save Network');
+    networkButton.addEventListener('click', () => {
+        function initialize() {
+            networkButton.disabled = true;
+        }
+        function finalize() {
+            networkButton.disabled = false;
+        }
+        save(filename, settings, vertices, areas, initialize, finalize, warn);
+    });
+
+    const imageButton = createButton('Save Image');
+    imageButton.addEventListener('click', () => {
+    });
+
+    const videoButton = createButton('Save Video');
+    videoButton.addEventListener('click', () => {
+    });
+    videoButton.disabled = true;
+
     const control = document.createElement('div');
-    control.appendChild(downloadButton);
+    control.appendChild(animationButton);
+    control.appendChild(settingsButton);
+    control.appendChild(networkButton);
+    control.appendChild(imageButton);
+    control.appendChild(videoButton);
     return control;
 }
--- src/save.js
+++ src/save.js
@@ -2,68 +2,76 @@ import pako from 'pako';
 import streamSaver from 'streamsaver';
 
 
-export default function (filename, settings, vertices, areas) {
-    const start = Date.now();
-    const stream = streamSaver.createWriteStream(filename);
+export default function (filename, settings, vertices, areas, initialize, finalize, warn) {
+    try {
+        const start = Date.now();
 
-    const writer = stream.getWriter();
-    let active = true;
+        initialize();
 
-    window.addEventListener('unload', () => {
-        active = false;
-        writer.abort();
-    });
+        const stream = streamSaver.createWriteStream(filename);
 
-    const encoder = new TextEncoder();
-    const deflate = new pako.Deflate({ gzip: true });
+        const writer = stream.getWriter();
+        let active = true;
 
-    deflate.onData = (chunk) => {
-        writer.write(chunk);
-    };
-    deflate.onEnd = (status) => {
-        if (status !== 0) {
-            throw deflate.msg;
+        window.addEventListener('unload', () => {
+            active = false;
+            writer.abort();
+        });
+
+        const encoder = new TextEncoder();
+        const deflate = new pako.Deflate({ gzip: true });
+
+        deflate.onData = (chunk) => {
+            writer.write(chunk);
+        };
+        deflate.onEnd = (status) => {
+            if (status !== 0) {
+                throw deflate.msg;
+            }
+            finalize();
+            console.log(`Saved in ${(Date.now() - start) / 1000} seconds`);
+        };
+        function push(type, data, props) {
+            data.type = type;
+            if (props !== null) {
+                data.props = props;
+            }
+            const line = `${JSON.stringify(data)}\n`;
+            deflate.push(encoder.encode(line), false);
         }
-        console.log(`Saved in ${(Date.now() - start) / 1000} seconds`);
-    };
-    function push(type, data, props) {
-        data.type = type;
-        if (props !== null) {
-            data.props = props;
-        }
-        const line = `${JSON.stringify(data)}\n`;
-        deflate.push(encoder.encode(line), false);
-    }
 
-    if (active) {
-        push('settings', {}, settings.props);
-    }
-
-    for (const [id, vertex] of Object.entries(vertices)) {
         if (active) {
-            push('vertex', { id }, { ...vertex.props, x: vertex.x, y: vertex.y });
+            push('settings', {}, settings.props);
         }
-    }
 
-    for (const [u, area] of Object.entries(areas)) {
-        if (active) {
-            for (const neighbor of Object.values(area.neighbors)) {
-                let source;
-                let target;
-                if (neighbor.reversed) {
-                    source = neighbor.v;
-                    target = u;
-                } else {
-                    source = u;
-                    target = neighbor.v;
-                }
-                push('edge', { source, target }, neighbor.props);
+        for (const [id, vertex] of Object.entries(vertices)) {
+            if (active) {
+                push('vertex', { id }, { ...vertex.props, x: vertex.x, y: vertex.y });
             }
         }
-    }
 
-    if (active) {
-        deflate.push(new Uint8Array(), true);
-        writer.close();
+        for (const [u, area] of Object.entries(areas)) {
+            if (active) {
+                for (const neighbor of Object.values(area.neighbors)) {
+                    let source;
+                    let target;
+                    if (neighbor.reversed) {
+                        source = neighbor.v;
+                        target = u;
+                    } else {
+                        source = u;
+                        target = neighbor.v;
+                    }
+                    push('edge', { source, target }, neighbor.props);
+                }
+            }
+        }
+
+        if (active) {
+            deflate.push(new Uint8Array(), true);
+            writer.close();
+        }
+    } catch (error) {
+        warn(error);
     }
 }
